ba46230449ac5155f5e2d013875f845f0f9e980d
Dasha Lary <dk2800@nyu.edu>
2020-09-10 21:45:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,10 @@
 def my_find(collection)
  i = 0
   while i < collection.length
-  if yield(collection[i])
+  if
+    yield(collection[i])
      return collection[i]
+   end
   i = i + 1
   end
-end
 end
